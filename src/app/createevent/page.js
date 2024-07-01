bf2bcd38ab4c2fb3ffb920c4fc17a91708587580
Amelia <ameliadevara@gmail.com>
2024-07-01 15:49:26 +0700

    update code dashboard/page.js and still on progress createevent/page.js

--- src/app/createevent/page.js
+++ src/app/createevent/page.js
@@ -1,4 +1,5 @@
 "use client";
+
 import { useActionState } from "react";
 import { createEventAction } from "./action";
 
--- src/app/dashboard/page.js
+++ src/app/dashboard/page.js
@@ -1,5 +1,5 @@
-import { createEvent } from "../[id]/createEvent";
 import { prisma } from "@/utils/prisma";
+import Link from "next/link";
 
 export default async function Home() {
   const events = await prisma.event.findMany({
@@ -13,10 +13,12 @@ export default async function Home() {
   });
 
   return (
-    <div>
-      <div className="mb-2">
-        <createEvent />
-      </div>
+    <div className="my-10 mx-10">
+      <Link href="/createevent/page">
+        <button class="inline-block cursor-pointer rounded-md bg-gray-800 px-4 py-3 text-center text-sm font-semibold uppercase text-white transition duration-200 ease-in-out hover:bg-gray-900 ml-auto">
+          Create Event
+        </button>
+      </Link>
       <table className="table w-full">
         <thead>
           <tr>
@@ -25,17 +27,17 @@ export default async function Home() {
             <th>Date</th>
             <th>Location</th>
             <th>Description</th>
-            <th className="text-center">Action</th>
+            <th>Action</th>
           </tr>
         </thead>
         <tbody>
           {events.map((event) => (
-            <tr key={event.id} event={event}>
+            <tr key={event.id}>
+              <td>{event.id}</td>
               <td>{event.title}</td>
-              <td>{event.date}</td>
+              <td>{new Date(event.date).toLocaleDateString()}</td>
               <td>{event.location}</td>
               <td>{event.description}</td>
-              <td></td>
             </tr>
           ))}
         </tbody>
